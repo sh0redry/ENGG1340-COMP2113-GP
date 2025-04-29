5e2b3c5082a711a2ae2d8e4d5fed783d5c07e75e
Yitong Li <u3628937@connect.hku.hk>
2025-04-29 22:39:24 +0800

    Remove constructor implementation from CounterBase class: Cleaned up code by eliminating the constructor definition, streamlining the class structure while maintaining functionality.

--- src/Counters/CounterBase.cpp
+++ src/Counters/CounterBase.cpp
@@ -4,9 +4,6 @@
 #include <iostream>
 #include <limits>
 
-CounterBase::CounterBase(Player& player, const std::string& name)
-    : m_player(player), m_name(name) {}
-
 const std::string& CounterBase::GetName() const {
     return m_name;
 }
